900a41412692d3f26a6d250ca831ad2bccc28be3
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-08-05 23:03:38 +0300

    Rearranged css and js code around to be cleaner and added a dark overlay for form popup.

--- index.js
+++ index.js
@@ -8,18 +8,104 @@ function Book(author, title, pages, hasRead) {
 }
 
 Book.prototype.updateHasRead = function () {
-  console.log('called');
   if (this.hasRead === true) {
-    console.log('1');
     this.hasRead = false;
   } else {
-    console.log('2');
     this.hasRead = true;
   }
 }
 
+function isInLibrary(author) {
+  myLibrary.forEach(function (value) {
+    if (value.author === author) {
+      return true;
+    }
+  })
+}
+
+function handleBookForm() {
+  formContainer.style.display = 'flex';
+  darkOverlay.style.display = 'block';
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let author = authorInput.value;
+    let title = titleInput.value;
+    let pages = pagesInput.value;
+    let hasRead = hasReadInput.checked;
+    addBookToLibrary(author, title, pages, hasRead);
+    formContainer.style.display = 'none';
+  })
+}
+
+function addBookToLibrary(author, title, pages, hasRead) {
+  if (!isInLibrary(author)) {
+    addNewBook(author, title, pages, hasRead);
+  }
+}
+
+function addNewBook(author, title, pages, hasRead) {
+  let newBookObj = new Book(author, title, pages, hasRead)
+  const newBook = document.createElement('div');
+  const newAuthor = document.createElement('p');
+  const newTitle = document.createElement('p');
+  const newPages = document.createElement('p');
+  const newHasRead = document.createElement('button');
+  const remove = document.createElement('button');
+
+  newBook.classList.add('book');
+  newAuthor.id = 'book-author';
+  newHasRead.classList.add('btn', 'has-read');
+  remove.classList.add('btn', 'remove-btn');
+
+  newHasRead.addEventListener('click', function () {
+    newBookObj.updateHasRead();
+    console.log(newBookObj.hasRead);
+    if (newBookObj.hasRead) {
+      newHasRead.textContent = 'yes';
+    } else {
+      newHasRead.textContent = 'no';
+    }
+  });
+
+  remove.addEventListener('click', function () {
+    removeBtn(remove);
+  })
+
+  newAuthor.textContent = author;
+  newTitle.textContent = title;
+  newPages.textContent = pages;
+
+  if (hasRead) {
+    newHasRead.textContent = 'yes';
+  } else {
+    newHasRead.textContent = 'no';
+  }
+
+  remove.textContent = 'remove';
+  
+  newBook.appendChild(newAuthor);
+  newBook.appendChild(newTitle);
+  newBook.appendChild(newPages);
+  newBook.appendChild(newHasRead);
+  newBook.appendChild(remove);
+
+  myLibrary.push(newBookObj);
+  booksContainer.appendChild(newBook);
+}
+
 const addBookBtn = document.querySelector('#add-book-btn');
 addBookBtn.addEventListener('click', handleBookForm);
+
+const formContainer = document.querySelector('.form-container');
+const darkOverlay = document.querySelector('.dark-overlay');
+const form = document.querySelector('#add-new-book-form');
+const authorInput = document.querySelector('#author');
+const titleInput = document.querySelector('#title');
+const pagesInput = document.querySelector('#pages');
+const hasReadInput = document.querySelector('#is-read');
+const booksContainer = document.querySelector('.books-container');
+
 
 function removeBtn(button) {
   let buttonParent = button.parentNode;
@@ -33,89 +119,3 @@ function removeBtn(button) {
   })
 }
 
-function isInLibrary(author) {
-  myLibrary.forEach(function (value) {
-    if (value.author === author) {
-      return true;
-    }
-  })
-}
-
-const formContainer = document.querySelector('.form-container');
-const form = document.querySelector('#add-new-book-form');
-const authorInput = document.querySelector('#author');
-const titleInput = document.querySelector('#title');
-const pagesInput = document.querySelector('#pages');
-const hasReadInput = document.querySelector('#is-read');
-
-function handleBookForm() {
-  formContainer.style.display = 'flex';
-
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let author = authorInput.value;
-    let title = titleInput.value;
-    let pages = pagesInput.value;
-    let hasRead = hasReadInput.checked;
-    // if (hasRead === true) {
-    //   hasRead = 'yes';
-    // } else {
-    //   hasRead = 'no';
-    // }
-    addBookToLibrary(author, title, pages, hasRead);
-    formContainer.style.display = 'none';
-  })
-}
-
-function addBookToLibrary(author, title, pages, hasRead) {
-  if (!isInLibrary(author)) {
-    addNewBook(author, title, pages, hasRead);
-  }
-}
-
-const booksContainer = document.querySelector('.books-container');
-
-function addNewBook(author, title, pages, hasRead) {
-  let newBookObj = new Book(author, title, pages, hasRead)
-  console.log(newBookObj.hasRead);
-  const newBook = document.createElement('div');
-  newBook.classList.add('book');
-  const newAuthor = document.createElement('p');
-  const newTitle = document.createElement('p');
-  const newPages = document.createElement('p');
-  const newHasRead = document.createElement('button');
-  newHasRead.addEventListener('click', function () {
-    newBookObj.updateHasRead();
-    console.log(newBookObj.hasRead);
-    if (newBookObj.hasRead) {
-      newHasRead.textContent = 'yes';
-    } else {
-      newHasRead.textContent = 'no';
-    }
-  });
-  const remove = document.createElement('button');
-  remove.addEventListener('click', function () {
-    removeBtn(remove);
-  })
-  newHasRead.classList.add('btn', 'has-read');
-  remove.classList.add('btn', 'remove-btn');
-  newAuthor.textContent = author;
-  newTitle.textContent = title;
-  newPages.textContent = pages;
-  if (hasRead) {
-    newHasRead.textContent = 'yes';
-  } else {
-    newHasRead.textContent = 'no';
-  }
-  remove.textContent = 'remove';
-  
-
-  newBook.appendChild(newAuthor);
-  newBook.appendChild(newTitle);
-  newBook.appendChild(newPages);
-  newBook.appendChild(newHasRead);
-  newBook.appendChild(remove);
-
-  myLibrary.push(newBookObj);
-  booksContainer.appendChild(newBook);
-}
