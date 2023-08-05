475d517f74ef911edd2db477369446a95152546d
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-08-05 17:25:44 +0300

    Added submit book form UI.

--- index.js
+++ index.js
@@ -7,23 +7,49 @@ function Book(author, title, pages, hasRead) {
   this.hasRead = hasRead;
 }
 
-let addBookBtn = document.querySelector('#add-book-btn');
+const addBookBtn = document.querySelector('#add-book-btn');
 addBookBtn.addEventListener('click', addBookToLibrary);
+
+function isInLibrary(author) {
+  myLibrary.forEach(function (value) {
+    if (value.author === author) {
+      return true;
+    }
+  })
+}
 
 function addBookToLibrary() {
   let author = prompt('What is the book author?');
   let title = prompt('What is the book title?');
   let pages = prompt('How many pages?');
   let hasRead = prompt('Have you read it? Yes/No');
-  myLibrary.push(new Book(author, title, pages, hasRead));
-  displayBooks();
+
+  if (!isInLibrary(author)) {
+    myLibrary.push(new Book(author, title, pages, hasRead));
+    addNewBook(author, title, pages, hasRead);
+  }
+
 }
 
-function displayBooks() {
-  myLibrary.forEach(function (value, index) {
-    console.log(value.author);
-    console.log(value.title);
-    console.log(value.pages);
-    console.log(value.hasRead);
-  })
+const booksContainer = document.querySelector('.books-container');
+
+function addNewBook(author, title, pages, hasRead) {
+    const newBook = document.createElement('div');
+    newBook.classList.add('book');
+    const newAuthor = document.createElement('p');
+    const newTitle = document.createElement('p');
+    const newPages = document.createElement('p');
+    const newHasRead = document.createElement('button');
+    newHasRead.classList.add('btn', 'has-read');
+    newAuthor.textContent = author;
+    newTitle.textContent = title;
+    newPages.textContent = pages;
+    newHasRead.textContent = hasRead;
+
+    newBook.appendChild(newAuthor);
+    newBook.appendChild(newTitle);
+    newBook.appendChild(newPages);
+    newBook.appendChild(newHasRead);
+
+    booksContainer.appendChild(newBook);
 }
