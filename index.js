44eca2b6125084e3f09bdb70b5929cac06f19198
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-08-05 17:48:24 +0300

    Added functionality for the form, so now a book is added when form is submitted.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function Book(author, title, pages, hasRead) {
 }
 
 const addBookBtn = document.querySelector('#add-book-btn');
-addBookBtn.addEventListener('click', addBookToLibrary);
+addBookBtn.addEventListener('click', handleBookForm);
 
 function isInLibrary(author) {
   myLibrary.forEach(function (value) {
@@ -18,17 +18,37 @@ function isInLibrary(author) {
   })
 }
 
-function addBookToLibrary() {
-  let author = prompt('What is the book author?');
-  let title = prompt('What is the book title?');
-  let pages = prompt('How many pages?');
-  let hasRead = prompt('Have you read it? Yes/No');
+const formContainer = document.querySelector('.form-container');
+const form = document.querySelector('#add-new-book-form');
+const authorInput = document.querySelector('#author');
+const titleInput = document.querySelector('#title');
+const pagesInput = document.querySelector('#pages');
+const hasReadInput = document.querySelector('#is-read');
 
+function handleBookForm() {
+  formContainer.style.display = 'flex';
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let author = authorInput.value;
+    let title = titleInput.value;
+    let pages = pagesInput.value;
+    let hasRead = hasReadInput.checked;
+    if (hasRead === true) {
+      hasRead = 'yes';
+    } else {
+      hasRead = 'no';
+    }
+    addBookToLibrary(author, title, pages, hasRead);
+    formContainer.style.display = 'none';
+  })
+}
+
+function addBookToLibrary(author, title, pages, hasRead) {
   if (!isInLibrary(author)) {
     myLibrary.push(new Book(author, title, pages, hasRead));
     addNewBook(author, title, pages, hasRead);
   }
-
 }
 
 const booksContainer = document.querySelector('.books-container');
